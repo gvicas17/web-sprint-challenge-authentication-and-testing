79cfc6f66270db134810f46a6788cc16613ec004
Gabby Vicas <68706544+gvicas17@users.noreply.github.com>
2021-01-29 13:57:45 -0500

    created first test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,30 @@
 // Write your tests here
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
+})
+
+const User = require('./auth/auth-model')
+const db = require('../data/dbConfig')
+
+const gabby = {username: 'gabbyVicas', password: 'password'}
+const password = {password: 'password'}
+
+beforeAll(async () => {
+  await db.migrate.rollback
+  await db.migrate.latest()
+})
+beforeEach(async () => {
+  await db('users').truncate()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
+describe('create new user', () => {
+  it('creates new user', async () => {
+    let users
+    await User.add(gabby)
+    users = await db('users').first()
+    expect(users).toMatchObject({ id: 1, ...gabby})
+  })
 })
