438617f9a781f8438456023d1a6c9fd09539b66b
Gabby Vicas <68706544+gvicas17@users.noreply.github.com>
2021-01-29 15:32:39 -0500

    fixed first 2 tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,13 +1,15 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(true)
-})
+
 
 const User = require('./auth/auth-model')
 const db = require('../data/dbConfig')
+const server = require('./server')
+const request = require('supertest')
+const router = require('./auth/auth-router')
 
 const gabby = {username: 'gabbyVicas', password: 'password'}
-const password = {password: 'password'}
+const gabbyv = {username: 'gup', password: 'password'}
+
 
 beforeAll(async () => {
   await db.migrate.rollback
@@ -20,11 +22,18 @@ afterAll(async () => {
   await db.destroy()
 })
 
+test('sanity', () => {
+  expect(true).toBe(true)
+})
+
 describe('create new user', () => {
   it('creates new user', async () => {
-    let users
-    await User.add(gabby)
-    users = await db('users').first()
-    expect(users).toMatchObject({ id: 1, ...gabby})
+    let res
+    res = await request(server).post('/api/auth/register').send(gabby)
+    expect(res.body).toMatchObject({ id: 1, username: 'gabbyVicas'})
+  })
+  it('sends 201 for successfully creating new user', async () => {
+    const res = await request(server).post('/api/auth/register').send(gabbyv)
+    expect(res.status).toBe(201)
   })
 })
